ac334346e1f50b55a945561dfbcb11954bc8c066
zhs9527 <59416652+zhs9527@users.noreply.github.com>
2020-11-12 23:48:37 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,129 +1,66 @@
 #! /bin/bash
-if [[ -z "${UUID}" ]]; then
-  UUID="4890bd47-5180-4b1c-9a5d-3ef686543112"
-fi
-
-if [[ -z "${AlterID}" ]]; then
-  AlterID="10"
-fi
-
-if [[ -z "${V2_Path}" ]]; then
-  V2_Path="/FreeApp"
-fi
-
-if [[ -z "${V2_QR_Path}" ]]; then
-  V2_QR_Code="1234"
-fi
-
 rm -rf /etc/localtime
 ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 date -R
 
-SYS_Bit="$(getconf LONG_BIT)"
-[[ "$SYS_Bit" == '32' ]] && BitVer='_linux_386.tar.gz'
-[[ "$SYS_Bit" == '64' ]] && BitVer='_linux_amd64.tar.gz'
+mkdir /trojan-go
+cd /trojan-go
+wget --no-check-certificate -qO 'trojango.zip' "https://github.com/p4gefau1t/trojan-go/releases/download/v0.8.2/trojan-go-linux-amd64.zip"
+ls
+unzip trojango.zip
+rm -rf trojango.zip
+ls
+chmod +x /trojan-go/*
 
-mkdir /v2raybin
-cd /v2raybin
-wget --no-check-certificate -qO 'v2ray.zip' "https://github.com/zxclown/v2ray-heroku-undone/raw/master/v2ray-linux-64.zip"
-ls
-unzip v2ray.zip
-rm -rf v2ray.zip
-ls
-chmod +x /v2raybin/*
-
-mkdir /caddybin
-ls
-cd /caddybin
-wget --no-check-certificate -qO 'caddy.tar.gz' "https://github.com/zxclown/v2ray-heroku-undone/raw/master/caddy_v1.0.3_linux_amd64.tar.gz"
-ls
-tar xvf caddy.tar.gz
-ls
-rm -rf caddy.tar.gz
-chmod +x caddy
-cd /root
-mkdir /wwwroot
-cd /wwwroot
-
-wget --no-check-certificate -qO 'demo.tar.gz' "https://github.com/zxclown/v2ray-heroku-undone/raw/master/demo.tar.gz"
-tar xvf demo.tar.gz
-rm -rf demo.tar.gz
-
-#cat <<-EOF > /v2raybin/v2ray-$V_VER-linux-$SYS_Bit/config.json
-cat <<-EOF > /v2raybin/config.json
+cat > /trojan-go/server.json <<-EOF
 {
-    "log":{
-        "loglevel":"warning"
+    "run_type": "server",
+    "local_addr": "0.0.0.0",
+    "local_port": "$(($RANDOM + 10000))",
+    "remote_addr": "127.0.0.1",
+    "remote_port": 80,
+    "password": [
+        "$PassWord"
+    ],
+    "log_level": 1,
+    "log_file": "${configTrojanGoLogFile}",
+    "ssl": {
+        "verify": true,
+        "verify_hostname": true,
+        "cert": "/etc/ssl/certs/cert.pem",
+        "key": "/etc/ssl/private/key.pem",
+        "key_password": "",
+	    "prefer_server_cipher": false,
+        "alpn": [
+            "http/1.1"
+        ],
+        "reuse_session": true,
+        "session_ticket": false,
+        "session_timeout": 600,
+        "plain_http_response": "",
+        "curves": "",
+        "dhparam": "",
+        "fingerprint": "chrome"
     },
-    "inbound":{
-        "protocol":"vmess",
-        "listen":"127.0.0.1",
-        "port":2333,
-        "settings":{
-            "clients":[
-                {
-                    "id":"${UUID}",
-                    "level":1,
-                    "alterId":${AlterID}
-                }
-            ]
-        },
-        "streamSettings":{
-            "network":"ws",
-            "wsSettings":{
-                "path":"${V2_Path}"
-            }
-        }
+    "tcp": {
+        "no_delay": true,
+        "keep_alive": true
     },
-    "outbound":{
-        "protocol":"freedom",
-        "settings":{
-        }
+    "websocket": {
+        "enabled": true,
+        "path": "/",
+        "host": ""
+    },
+    "mysql": {
+        "enabled": false,
+        "server_addr": "127.0.0.1",
+        "server_port": 3306,
+        "database": "trojan",
+        "username": "trojan",
+        "password": ""
     }
 }
 EOF
 
-cat <<-EOF > /caddybin/Caddyfile
-http://0.0.0.0:${PORT}
-{
-	root /wwwroot
-	index index.html
-	timeouts none
-	proxy ${V2_Path} localhost:2333 {
-		websocket
-		header_upstream -Origin
-	}
-}
-EOF
-
-cat <<-EOF > /v2raybin/vmess.json
-{
-    "v": "2",
-    "ps": "${AppName}.herokuapp.com",
-    "add": "${AppName}.herokuapp.com",
-    "port": "443",
-    "id": "${UUID}",
-    "aid": "${AlterID}",
-    "net": "ws",
-    "type": "none",
-    "host": "",
-    "path": "${V2_Path}",
-    "tls": "tls"
-}
-EOF
-
-if [ "$AppName" = "no" ]; then
-  echo "不生成二维码"
-else
-  mkdir /wwwroot/$V2_QR_Path
-  vmess="vmess://$(cat /v2raybin/vmess.json | base64 -w 0)"
-  Linkbase64=$(echo -n "${vmess}" | tr -d '\n' | base64 -w 0)
-  echo "${Linkbase64}" | tr -d '\n' > /wwwroot/$V2_QR_Path/index.html
-  echo -n "${vmess}" | qrencode -s 6 -o /wwwroot/$V2_QR_Path/v2.png
-fi
-
-#cd /v2raybin/v2ray-$V_VER-linux-$SYS_Bit
-cd /v2raybin
-./v2ray &
-cd /caddybin
-./caddy -conf="Caddyfile"
+cd /trojan-go
+./trojan-go -config /trojan-go/server.json
